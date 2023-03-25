6639e50713bb395044e8d02ec1f805800f93e231
Nando <luis27.2000.Fernando@gmail.com>
2023-03-25 16:33:51 -0500

    Maps Base Int

---
+++ app/src/main/java/com/example/test3/usecase/map/MapFragment.java
@@ -0,0 +1,23 @@
+package com.example.test3.usecase.map;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.test3.R;
+
+public class MapFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
